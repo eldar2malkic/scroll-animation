d6b4779ed85b25d924aa11c8cf2cec26c5e8599d
eldar2malkic <88284877+eldar2malkic@users.noreply.github.com>
2023-06-01 17:38:53 -0500

    Final commit

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const boxses = document.querySelectorAll('.box')
+const boxes = document.querySelectorAll('.box');
 
-window.addEventListener('scroll', checkBoxes)
+window.addEventListener('scroll', checkBoxes);
 
 checkBoxes()
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4
-
-    boxses.forEach(box => {
+    const triggerBotton = window.innerHeight / 5 * 4;
+    boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
 
-        if(boxTop < triggerBottom) {
+        if(boxTop < triggerBotton) {
             box.classList.add('show')
         } else {
             box.classList.remove('show')
